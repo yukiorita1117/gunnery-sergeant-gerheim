cce95fbfa22d871d5b3a4c4ed5496dc1e6ef75be
折田幸夫 <3204704@gmail.com>
2020-04-18 18:35:54 +0900

    InputForm 作成

---
+++ app/src/components/InputForm.jsx
@@ -0,0 +1,54 @@
+// TODO create のため。一旦雛形状態。
+
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+// import { requestData, receiveDataSuccess, receiveDataFailed } from "../actions";
+
+const InputForm = ({ store }) => {
+  //   const { isFetching, resultArray } = store.getState().fetchData;
+
+  const handleFetchData = () => {
+    // store.dispatch(requestData()); // axios.get()を呼ぶ前にisFetchingをtrueに！
+    axios
+      .get("/api/create")
+      .then((res) => {
+        // データ受け取りに成功
+        // const _resultArray = response.data;
+        // store.dispatch(receiveDataSuccess(_resultArray)); // データをstoreに保存するとともにisFetchingをfalseに
+      })
+      .catch((err) => {
+        // データ受け取りに失敗
+        // console.error(new Error(err));
+        // store.dispatch(receiveDataFailed()); // isFetchingをfalseに
+      });
+  };
+
+  const Item = styled.li`
+    list-style: none;
+  `;
+
+  return (
+    <>
+      <br />
+      <div>
+        {isFetching ? (
+          // データをFetch中ならばローディングアイコンを表示
+          <h2>Now Loading...</h2>
+        ) : (
+          <div>
+            <button onClick={() => handleFetchData()}>fetch data</button>
+            <ul>
+              {resultArray.map((result) => (
+                <Item key={result.id}>{`${result.inputText}`}</Item>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <br />
+    </>
+  );
+};
+
+export default InputForm;
